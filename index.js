1f7015d677af2eed5cac76b4c2a6f7a1377a6308
Alejandro Ortiz <aleortbas@gmail.com>
2023-02-19 18:51:03 -0500

    Authentication not tested

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -32,6 +33,12 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTE */
+app.use("/auth", authRoutes);
 
 /* MOONGOSE SETUP */
 
